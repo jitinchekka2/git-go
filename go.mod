ad9d4910d3538a44138061a815ad95b1a121ba42
Jitin Chekka <cjkrishna@aqfer.com>
2024-06-19 18:44:39 +0530

    initial

---
+++ go.mod
@@ -0,0 +1,15 @@
+module git-go
+
+go 1.22.1
+
+require (
+	github.com/google/go-github/v29 v29.0.3
+	github.com/shurcooL/githubv4 v0.0.0-20240429030203-be2daab69064
+	golang.org/x/oauth2 v0.21.0
+)
+
+require (
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/shurcooL/graphql v0.0.0-20230722043721-ed46e5a46466 // indirect
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/google/go-github/v29/github"
+	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
+)
+
+type gitImpl struct {
+	owner       string
+	repo        string
+	branch      string
+	accessToken string
+	client      *github.Client
+	gqlClient   *githubv4.Client
+}
+
+func (g *gitImpl) gQLClient() *githubv4.Client {
+	if g.gqlClient == nil {
+		ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: g.accessToken})
+		tc := oauth2.NewClient(context.Background(), ts)
+		g.gqlClient = githubv4.NewClient(tc)
+	}
+	return g.gqlClient
+}
+
+func (g *gitImpl) DeleteBranch(branchName string) error {
+	client := g.gQLClient()
+
+	// Define GraphQL query to find the Ref ID
+	var q struct {
+		Repository struct {
+			Ref struct {
+				ID githubv4.ID
+			} `graphql:"ref(qualifiedName: $ref)"`
+		} `graphql:"repository(owner: $owner, name: $name)"`
+	}
+
+	variables := map[string]interface{}{
+		"name":  githubv4.String(g.repo),
+		"owner": githubv4.String(g.owner),
+		"ref":   githubv4.String("refs/heads/" + branchName),
+	}
+
+	if err := client.Query(context.Background(), &q, variables); err != nil {
+		return err
+	}
+
+	// Define GraphQL mutation to delete branch
+	var mutation struct {
+		DeleteRef struct {
+			ClientMutationId githubv4.String
+		} `graphql:"deleteRef(input: $input)"`
+	}
+
+	input := githubv4.DeleteRefInput{
+		RefID: q.Repository.Ref.ID,
+	}
+
+	if err := client.Mutate(context.Background(), &mutation, input, nil); err != nil {
+		return fmt.Errorf("error deleting branch: %w", err)
+	}
+	return nil
+}
+func main() {
+	cl := gitImpl{
+		owner:       "jitinchekka2",
+		repo:        "git-go",
+		branch:      "branch-1",
+		accessToken: os.Getenv("GITHUB_TOKEN"),
+	}
+	if err := cl.DeleteBranch("branch-1"); err != nil {
+		fmt.Println(err)
+	}
+}
+
+// Access token: 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
